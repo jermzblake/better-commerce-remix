388dcab9f1788eca9eb8ab13ecff9ee212f97e25
jermzblake <jermzblake@gmail.com>
2023-06-19 08:25:52 -0400

    create navBar

---
+++ app/components/navbar/navBar.tsx
@@ -0,0 +1,35 @@
+import { Link, Form } from '@remix-run/react'
+import { useSearchParams, useNavigate } from "react-router-dom"
+
+export const NavBar = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const navigate = useNavigate()
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchParams({ search: e.target.value })
+    navigate(`?search=${e.target.value}`)
+  }
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <div className="nav-section">
+        <div>
+          <input
+            type="search"
+            placeholder="Search..."
+            value={searchParams.get('search') || ''}
+            onChange={handleChange}
+          />
+        </div>
+        <div>cart</div>
+      </div>
+    </nav>
+  )
+}
