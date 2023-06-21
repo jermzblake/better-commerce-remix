129d80dd6ebf603d44722da0af3b3481d09cdb4f
jermzblake <jermzblake@gmail.com>
2023-06-20 21:54:56 -0400

    add HeroSlider and NavBar to index route

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,60 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from "@remix-run/node"
+import { NavBar } from "~/components/navbar/navBar"
+import type { LinksFunction } from '@remix-run/node'
+import stylesUrl from '~/styles/index.css'
+import { HeroSlider, SliderItem } from '../components/slider/heroSlider'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: stylesUrl }]
+}
+const slides = [
+  {
+    imageUrl: "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    heading: "Slide 1",
+    tagline: "Tagline1",
+    ctaText: "CTA"
+  },
+  {
+    imageUrl: "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    heading: "Slide 2",
+    tagline: "Tagline2",
+    ctaText: "CTA"
+  },
+  {
+    imageUrl: "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    heading: "Slide 3",
+    tagline: "Tagline3",
+    ctaText: "CTA"
+  }
+]
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Better Commerce" },
+    { name: "description", content: "Welcome to Better Commerce" },
   ];
 };
 
-export default function Index() {
+const Index = () => {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="page-container">
+      <NavBar />
+      <HeroSlider slidesCount={slides.length}>
+        {slides.map((slide, index) => (
+          <SliderItem key={index} width="100%">
+            <h2>{slide.heading}</h2>
+            <img src={slide.imageUrl} alt="hero slide" style={{ objectFit: "contain", objectPosition: "center", height: "50vh"}} />
+            <p>{slide.tagline}</p>
+          </SliderItem>
+        ))}
+      </HeroSlider>
+      <header className="hero">
+        <h1>Better Commerce</h1>
+      </header>
+      <section>
+      </section>
     </div>
   );
 }
+
+export default Index
