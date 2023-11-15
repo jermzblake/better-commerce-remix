58da3b51e0ad695d7d8c14eb6b21e85c7a7bec31
jermzblake <jermzblake@gmail.com>
2023-11-14 21:38:41 -0500

    add checkout form

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,25 +1,254 @@
-import { Link, useLoaderData } from "@remix-run/react"
+import { Link, useLoaderData, useFetcher } from "@remix-run/react"
 import type { LoaderFunction } from "@remix-run/node"
 import { json } from "@remix-run/node"
 import { NavBar } from "~/components/navbar/navBar"
 import type { LinksFunction } from '@remix-run/node'
 import stylesUrl from '~/styles/checkout.css'
+import { shoppingCartCookie } from '~/cookie.server'
+import type { CartProduct } from "~/common/types"
+import { useState } from "react"
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: stylesUrl }]
 }
 
-export const loader: LoaderFunction = async () => {
-  return null
+export const loader: LoaderFunction = async ({ request }) => {
+  const cookieHeader = request.headers.get("Cookie")
+  const cookie = (await shoppingCartCookie.parse(cookieHeader)) || []
+  return json(cookie)
 }
 
 const CheckoutRoute = () => {
-  const items = useLoaderData()
+  const items: CartProduct[] = useLoaderData()
+  const fetcher = useFetcher()
+  const [isSameAddress, setIsSameAddress] = useState(false)
+  const [checkoutForm, setCheckoutForm] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    address: '',
+    city: '',
+    province: '',
+    country: '',
+    postalCode: '',
+    phoneNumber: '',
+    note: '',
+    shippingAddress: '',
+    shippingProvince: '',
+    shippingCity: '',
+    shippingCountry: '',
+    shippingPostalCode: '',
+    shippingPhoneNumber: '',
+    coupon: '',
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    setCheckoutForm({ ...checkoutForm, [name]: value })
+  }
+
   return (
     <div className="page-container">
       <NavBar />
       <div>
-        <h1>This is the Checkout page</h1>
+        <h1>Checkout</h1>
+        <fetcher.Form method="post">
+          <div>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="First Name"
+              value={checkoutForm.firstName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="Last Name"
+              value={checkoutForm.lastName}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+              value={checkoutForm.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Address"
+              value={checkoutForm.address}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="country">Country</label>
+            <input
+              type="text"
+              id="country"
+              name="country"
+              placeholder="Country"
+              value={checkoutForm.country}
+              onChange={handleChange}
+            >
+            </input>
+          </div>
+          <div>
+            <label htmlFor="province">Province/State</label>
+            <input
+              type="text"
+              id="province"
+              name="province"
+              placeholder="Province/State"
+              value={checkoutForm.province}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              id="city"
+              name="city"
+              placeholder="City"
+              value={checkoutForm.city}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="postalCode">Postal Code</label>
+            <input
+              type="text"
+              id="postalCode"
+              name="postalCode"
+              placeholder="Postal Code"
+              value={checkoutForm.postalCode}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="phone">Phone</label> 
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              placeholder="Phone"
+              value={checkoutForm.phoneNumber}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="note">Note</label>
+            <textarea
+              id="note"
+              name="note"
+              placeholder="Note"
+              rows={3}
+              maxLength={200}
+              value={checkoutForm.note}
+              // onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="isSameAddress">Same as shipping address</label>
+            <input
+              type="checkbox"
+              id="isSameAddress"
+              name="isSameAddress"
+              onChange={() => setIsSameAddress(!isSameAddress)}
+            />
+          </div>
+          {!isSameAddress && (
+            <>
+          <div>
+            <label htmlFor="shipping">Shipping Address</label>
+            <input
+              type="text"
+              id="shipping"
+              name="shipping"
+              placeholder="Shipping Address"
+              value={checkoutForm.shippingAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="shippingCountry">Shipping Country</label>
+            <input
+              type="text"
+              id="shippingCountry"
+              name="shippingCountry"
+              placeholder="Shipping Country"
+              value={checkoutForm.shippingCountry}
+              onChange={handleChange}
+            >
+            </input>
+          </div>
+          <div>
+            <label htmlFor="shippingProvince">Shipping Province/State</label>
+            <input
+              type="text"
+              id="shippingProvince"
+              name="shippingProvince"
+              placeholder="Shipping Province/State"
+              value={checkoutForm.shippingProvince}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="shippingCity">Shipping City</label>
+            <input
+              type="text"
+              id="shippingCity"
+              name="shippingCity"
+              placeholder="Shipping City"
+              value={checkoutForm.shippingCity}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="shippingPostalCode">Shipping Postal Code</label>
+            <input
+              type="text"
+              id="shippingPostalCode"
+              name="shippingPostalCode"
+              placeholder="Shipping Postal Code"
+              value={checkoutForm.shippingPostalCode}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="coupon">Coupon</label>
+            <input
+              type="text"
+              id="coupon"
+              name="coupon"
+              placeholder="Coupon"
+              value={checkoutForm.coupon}
+              onChange={handleChange}
+            />
+          </div>
+          </>
+          )}
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </fetcher.Form>
       </div>
     </div>
   )
