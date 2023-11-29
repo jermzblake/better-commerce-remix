93a925246151c771d7d012cce3b5f1749be3f277
jermzblake <jermzblake@gmail.com>
2023-11-28 22:13:20 -0500

    add clearShoppingCarCookie method

--- app/cookie.server.ts
+++ app/cookie.server.ts
@@ -3,3 +3,7 @@ import { createCookie } from '@remix-run/node'
 export const shoppingCartCookie = createCookie('shopping_cart', {
   maxAge: 604_800, // one week
 })
+
+export const clearShoppingCartCookie = () => {
+  return shoppingCartCookie.serialize([])
+}
